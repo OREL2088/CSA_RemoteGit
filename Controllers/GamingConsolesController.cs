72dc6deaafc3f9994beb1b84a0520e2f8f55ab7a
OREL <slavik2088@gmail.com>
2023-06-05 17:51:19 +0700

    Initial commit for major files

---
+++ Controllers/GamingConsolesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ClientServe.Models;
+
+namespace ClientServ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamingConsolesController : ControllerBase
+    {
+        private static List<GamingСonsoles> _Console = new List<GamingСonsoles>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<GamingСonsoles>> Get()
+        {
+            return _Console;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<GamingСonsoles> Get(int id)
+        {
+            if (_Console.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            return _Console[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] GamingСonsoles value)
+        {
+            _Console.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] GamingСonsoles value)
+        {
+            if (_Console.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            _Console[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            if (_Console.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            _Console.RemoveAt(id);
+        }
+    }
+}
---
+++ Models/GamingСonsoles.cs
@@ -0,0 +1,11 @@
+﻿namespace ClientServe.Models
+{
+    public class GamingСonsoles
+    {
+        public string Firm { get; set; }
+        public string Model { get; set; }
+        public int Memory { get; set; }
+        public int Price { get; set; }
+
+    }
+}
